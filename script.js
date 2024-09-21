daf9cc96fe7dadbdaaa632f1842e3d57ccca7e70
Raul Kevin Quispe Diaz <raul.g.quispe@gmail.com>
2024-09-21 18:59:08 -0500

    actualización

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-onload = () => {
+
+window.onload = function () {
   document.body.classList.remove("container");
-};
+  console.log("Iniciando script de reproducción de audio");
+
+  const audio = document.getElementById('background-music');
+
+  // Verifica si el elemento de audio se encontró correctamente
+  if (!audio) {
+    console.error("El elemento de audio no se encontró en el DOM.");
+    return;
+  }
+
+  const startTime = 0;
+  const endTime = 80;
+  function playSegment() {
+    audio.currentTime = startTime;
+    // REPRODUCIR LA MUSICA
+    audio.play().catch(error => {
+      console.error("Error al reproducir la música", error);
+    });
+    if (audio.currentTime >= endTime) {
+      audio.pause(); // Pausar el audio al alcanzar el final del segmento
+      playSegment(); // Repetir el segmento
+    }
+  }
+
+
+  playSegment();
+}
